84e18efff67e8c6fd2fdbca0351fd035d086a939
Lucas Cepeda <lucas.cepeda2@gmail.com>
2024-03-01 13:09:30 -0300

    ni idea que hice

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
 import Carrousel from './components/Carrousel/Carrousel';
 import Filters from './components/Filters/Filters'
-import CardProducto from './components/CardProducto/CardProducto';
+// import CardProducto from './components/CardProducto/CardProducto';
 import Footer from './components/Footer/Footer';
 
 
@@ -16,7 +16,7 @@ function App() {
       <Banner />
       <Carrousel />
       <Filters />
-      <CardProducto />
+      {/* <CardProducto /> */}
       <Footer />
 
     </>
--- src/components/CardProducto/CardProducto.jsx
+++ src/components/CardProducto/CardProducto.jsx
@@ -1,27 +1,90 @@
-import './cardproducto.css';
+// import './cardproducto.css';
+// import React, { useEffect, useState } from 'react';
 
-function CardProducto() {
+
+// async function getProductos() {
+//     let productos;
+    
+//     let response;
+//     try {
+//         response = await gapi.client.sheets.spreadsheets.values.get({
+//             spreadsheetId: '1PjlBRfdwQmTLgTZCes1evkAWpJL7pvm8CYdxAz6A5J0',
+//             range: 'Majors!A:G',
+//         });
+//     } catch (err) {
+//         console.error(err)
+//         return;
+//     }
+//     const range = response.result;
+//     if (!range || !range.values || range.values.length == 0) {
+//         console.warn("No se encontraron valores")
+//         return;
+//     }
+
+//     productos = [];
+//     range.values.forEach((fila) => {
+//         if (isNaN(parseInt(fila[0]))) return;
+//         const nuevoProducto = {
+//             id: fila[0],
+//             cliente: fila[1],
+//             email: fila[2],
+//             modelo: fila[3],
+//             problema: fila[4],
+//             fecha_terminado: fila[5],
+//             extracurricular: fila[6]
+//         };
+//         productos.push(nuevoProducto);
+//     });
+//     console.log(productos)
+// }
+
+// function CardProducto() {
+//     const [productos, setProductos] = useState([]);
+
+//     useEffect(() => {
+//         getProductos().then((productos) => {
+//             setProductos(productos);
+//         });
+//     }, []);
+
+//     return (
+//         <div id="productosContainer">
+//             {/* Mapea los productos y devuelve un elemento para cada uno */}
+//             {productos.map((producto, index) => (
+//                 <div key={index}>
+//                     <p>ID: {producto.id}</p>
+//                     <p>Cliente: {producto.cliente}</p>
+//                     <p>Email: {producto.email}</p>
+//                     <p>Modelo: {producto.modelo}</p>
+//                     <p>Problema: {producto.problema}</p>
+//                     <p>Fecha terminado: {producto.fecha_terminado}</p>
+//                     <p>Extracurricular: {producto.extracurricular}</p>
+//                     <hr />
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
+
+// export default CardProducto;
 
 
-return (
-    <div className='cardProducto'>
-        <div>
-                <img src='../src/assets/Products/sierraBremen.png' alt='logoFooter' />
-                <hr />
-                <div className="textos">
-                    <div className="nameProduct">Sierra Circular con Dientes de Widia</div>
-                    <div className="precios">
-                        <div className="precioLista">$2000</div>
-                        <div className="precioRebaja">$1500</div>
-                    </div>
-                    <div className="cuotas">6 cuotas sin interes de $250</div>
-                </div>
-        </div>
-    </div>
-    )
 
-}
 
-export default CardProducto;
 
+
+    // <div className='cardProducto'>
+    //     <div>
+    //             <img src='../src/assets/Products/sierraBremen.png' alt='logoFooter' />
+    //             <hr />
+    //             <div className="textos">
+    //                 <div className="nameProduct">Sierra Circular con Dientes de Widia</div>
+    //                 <div className="precios">
+    //                     <div className="precioLista">$2000</div>
+    //                     <div className="precioRebaja">$1500</div>
+    //                 </div>
+    //                 <div className="cuotas">6 cuotas sin interes de $250</div>
+    //             </div>
+    //     </div>
+    // </div>
 
